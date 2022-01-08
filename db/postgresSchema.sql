6f79f3ee221694039e93baa4b70c7bc3be9dc68b
Jason Adams <jasonadams1326@gmail.com>
2022-01-08 11:25:32 -0600

    removed comments from postgresSchema

--- db/postgresSchema.sql
+++ db/postgresSchema.sql
@@ -1,8 +1,3 @@
--- run in postgres terminal, then close
--- DROP DATABASE IF EXISTS sdc;
--- DROP DATABASE sdc WITH (FORCE);
--- CREATE DATABASE sdc;
-
 CREATE TABLE products (
   id INT PRIMARY KEY,
   name VARCHAR,
@@ -17,11 +12,6 @@ CREATE TABLE related_products (
   current_product_id INT,
   related_product_id INT
 );
--- CREATE INDEX idx_related_products
--- ON related_products(current_product_id);
-
--- CONSTRAINT fk_related_products_products
- -- FOREIGN KEY (current_product_id) REFERENCES products(id)
 
 CREATE TABLE features (
   id INT PRIMARY KEY,
@@ -29,24 +19,15 @@ CREATE TABLE features (
   feature VARCHAR,
   value VARCHAR
 );
--- CREATE INDEX idx_product_features
--- ON features(product_id);
-
--- CONSTRAINT fk_features_products FOREIGN KEY (id) REFERENCES products(id)
 
 CREATE TABLE styles (
   id INT PRIMARY KEY,
   product_id INT,
   name VARCHAR,
   sale_price VARCHAR,
-  -- changed sale_price text to varchar, need to reload and copy data
   original_price INT,
   default_style INT
 );
--- CREATE INDEX idx_product_styles
--- ON styles(product_id);
-
-  -- CONSTRAINT fk_styles_products FOREIGN KEY (id) REFERENCES products(id)
 
 CREATE TABLE photos (
   id INT PRIMARY KEY,
@@ -54,10 +35,6 @@ CREATE TABLE photos (
   url VARCHAR,
   thumbnail_url VARCHAR
 );
--- CREATE INDEX idx_photo_style
--- ON photos(styleId);
-
-  -- CONSTRAINT fk_photos_styles FOREIGN KEY (id) REFERENCES styles(id)
 
 CREATE TABLE skus (
   id INT PRIMARY KEY,
@@ -65,21 +42,3 @@ CREATE TABLE skus (
   size VARCHAR,
   quantity INT
 );
--- CREATE INDEX idx_sku_style
--- ON skus(styleId);
-
--- CONSTRAINT fk_skus_styles FOREIGN KEY (id) REFERENCES styles(id)
-
--- copy statements--
--- COPY products FROM '/Users/johndoe/sdc/product/productData/product.csv' DELIMITER ',' CSV HEADER;
--- COPY related_products FROM '/Users/johndoe/sdc/product/productData/related.csv' DELIMITER ',' CSV HEADER;
--- COPY features FROM '/Users/johndoe/sdc/product/productData/features.csv' DELIMITER ',' CSV HEADER;
--- COPY styles FROM '/Users/johndoe/sdc/product/productData/styles.csv' DELIMITER ',' CSV HEADER;
--- COPY photos FROM '/Users/johndoe/sdc/product/productData/photos.csv' DELIMITER ',' CSV HEADER;
--- COPY skus FROM '/Users/johndoe/sdc/product/productData/skus.csv' DELIMITER ',' CSV HEADER;\dt
-
-
-
-
-
-
